97dfa69b170ec637e9e44a7c9c0af16288c5f055
Natleak <129502071+Natleak@users.noreply.github.com>
2024-02-29 15:31:20 +0100

    complete ex 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (word, nbRepetition) {
+  let string = "";
+  if (nbRepetition < 0) {
+    return `ERROR`;
+  } else {
+    for (let i = 0; i < nbRepetition; i++) {
+      string = string + word;
+    }
+  }
+  return string;
 };
 
 // Do not edit below this line
