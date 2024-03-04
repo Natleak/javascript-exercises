275edc7b8f4aec50adb6964c7a39eff2c3d4ef92
Natleak <129502071+Natleak@users.noreply.github.com>
2024-03-04 10:42:49 +0100

    complete ex 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function (first, last) {
+  if (first > last) {
+    const min = first;
+    first = last;
+    last = min;
+  }
+  if (first < 0 || last < 0) {
+    return "ERROR";
+  }
+  if (!Number.isInteger(first) || !Number.isInteger(last)) {
+    return "ERROR";
+  }
+  let sum = 0;
+  for (let i = first; i <= last; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
